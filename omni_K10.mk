e59e93222afcaf0729574c8eed3c0215722b4b9d
androidlover5842 <androidlover5842@gmail.com>
2018-04-11 13:18:29 +0530

    initial commit

---
+++ omni_K10.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_COPY_FILES += device/OUKITEL/K10/kernel:kernel
+
+PRODUCT_DEVICE := K10
+PRODUCT_NAME := omni_K10
+PRODUCT_BRAND := OUKITEL
+PRODUCT_MODEL := K10
+PRODUCT_MANUFACTURER := OUKITEL
